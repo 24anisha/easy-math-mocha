10d4ed1e935a1ebff52307abf09b47789bcbfb88
24anisha <123.anisha@gmail.com>
2023-08-23 19:25:04 -0400

    added subtract with different function instantiation

--- src/arithmetic.js
+++ src/arithmetic.js
@@ -1,5 +1,11 @@
 const add = (a, b) => a + b
-const subtract = (a, b) => a - b
+// const subtract = (a, b) => a - b
+
+function subtract(a, b) {
+    let difference = a - b;
+    return difference;
+}
+
 const multiply = (a, b) => a * b
 const divide = (a, b) => b !== 0 ? (a / b) : undefined 
 module.exports = {
